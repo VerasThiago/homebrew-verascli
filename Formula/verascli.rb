fc1b5a6dcaeabebd529a567e06cb3e5d2e6bce81
Thiago Veras <thiago.veras.machado@gmail.com>
2020-11-16 18:13:48 -0300

    New download url

--- Formula/verascli.rb
+++ Formula/verascli.rb
@@ -1,8 +1,8 @@
 class Verascli < Formula
   desc "CLI tool for creating and managing VTEX apps"
   homepage "https://github.com/VerasThiago/verascli"
-  url "https://github.com/VerasThiago/verascli/releases/tag/v2.119.1"
-  sha256 "20ac8b1a9fe2e81083cb36cb2401a6ef1eee672f16e37bf6cce20ee048664d95"
+  url "https://github.com/VerasThiago/verascli/releases/download/v2.119.1/verascli-v2.119.1-darwin-x64.tar.gz"
+  sha256 "90181447004c59703ae11c0e861e3893b8d74177872b78a927bdc9689801aa2a"
   license "MIT"
 
   def install
