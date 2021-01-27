b4aef3540ea5ffedb3c1c760c4240173f21690af
Thiago Veras <thiago.veras.machado@gmail.com>
2021-01-27 20:09:36 +0000

    [Automatic] Release v2.121.2

--- Formula/verascli.rb
+++ Formula/verascli.rb
@@ -1,9 +1,9 @@
 class Verascli < Formula
   desc "CLI tool for creating and managing VTEX apps"
   homepage "https://github.com/VerasThiago/verascli"
-  url "https://vtex-toolbelt-test.s3.amazonaws.com/vtex-v2.121.1/vtex-v2.121.1-darwin-x64.tar.gz"
-  version "2.121.1"
-  sha256 "b2cd27f1f333f4ec1d9e62971905e6f8d862625cf8503b5eafd2c0a32f120584"
+  url "https://vtex-toolbelt-test.s3.amazonaws.com/vtex-v2.121.2/vtex-v2.121.2-darwin-x64.tar.gz"
+  version "2.121.2"
+  sha256 "410baa95cc15ef457c80b87b000f9755d53d1c492226a0bdb921a1a2389d0064"
   license "MIT"
 
   def install
